365c4fd3243e6f0632af37963b4181dfd4f76778
Noah Held <noahheld17@gmail.com>
2023-12-20 00:18:41 +0000

    Pass cancellation reasons through the scheduler
    Signed-off-by: Noah Held <noahheld17@gmail.com>

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -595,7 +595,10 @@ func (s *Scheduler) generateUpdateMessagesFromJob(job *jobdb.Job, jobRunErrors m
 		cancel := &armadaevents.EventSequence_Event{
 			Created: s.now(),
 			Event: &armadaevents.EventSequence_Event_CancelledJob{
-				CancelledJob: &armadaevents.CancelledJob{JobId: jobId},
+				CancelledJob: &armadaevents.CancelledJob{
+					JobId:  jobId,
+					Reason: job.CancelReason(),
+				},
 			},
 		}
 		events = append(events, cancel)
@@ -613,7 +616,10 @@ func (s *Scheduler) generateUpdateMessagesFromJob(job *jobdb.Job, jobRunErrors m
 		cancel := &armadaevents.EventSequence_Event{
 			Created: s.now(),
 			Event: &armadaevents.EventSequence_Event_CancelledJob{
-				CancelledJob: &armadaevents.CancelledJob{JobId: jobId},
+				CancelledJob: &armadaevents.CancelledJob{
+					JobId:  jobId,
+					Reason: job.CancelReason(),
+				},
 			},
 		}
 		events = append(events, cancelRequest, cancel)
@@ -850,7 +856,7 @@ func (s *Scheduler) cancelQueuedJobsIfExpired(txn *jobdb.Txn) ([]*armadaevents.E
 			return nil, err
 		}
 
-		reason := "Expired queue ttl"
+		reason := "Queue TTL expired"
 		cancel := &armadaevents.EventSequence{
 			Queue:      job.Queue(),
 			JobSetName: job.Jobset(),
